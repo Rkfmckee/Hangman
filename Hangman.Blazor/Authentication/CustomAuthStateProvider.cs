a280098ab88d4cb82b9287d8df048c8798270234
Ryan McKeever <rkfmckee@gmail.com>
2024-03-03 15:20:42 +0000

    Require authorization on Index page

---
+++ Hangman.Blazor/Authentication/CustomAuthStateProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Hangman.Blazor.Authentication
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var staticToken = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjZiYmZmMmVlLTY2MWItNGE2MC01ZmQ1LTA4ZGMzYWUxOGE3YiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiJ0ZXN0aW5nMTIzIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiUGxheWVyIiwiZXhwIjoxNzA5NTY1OTc3fQ.iLqIjhzazEYYt_Uied4ePLSN7BAJVEKK1xl0fOH-4vWJ6iE-V2peYPP4EhZwegK0J5iXj1UydvoHTEW57z-euQ";
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.ReadJwtToken(staticToken);
+
+            var identity = new ClaimsIdentity(token.Claims, "jwt");
+            //var identity = new ClaimsIdentity();
+            var user = new ClaimsPrincipal(identity);
+            var state = new AuthenticationState(user);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+            return state;
+        }
+    }
+}
--- Hangman.Blazor/Program.cs
+++ Hangman.Blazor/Program.cs
@@ -1,6 +1,8 @@
 using Hangman.Blazor;
+using Hangman.Blazor.Authentication;
 using Hangman.Blazor.Interfaces;
 using Hangman.Blazor.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,6 +11,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7061") });
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddAuthorizationCore();
 
 builder.Services.AddScoped<IGameService, GameService>();
 
